26511faeed92c9b4695e9e845695c3e39273b588
collingreen-del <collingreen@u.boisestate.edu>
2026-03-10 16:57:46 -0600

    Rename module from simple_4_mux to top

--- top.v
+++ top.v
@@ -1,13 +1,14 @@
-module simple_4_mux(
+module top(
     input [1:0] Sel,
     input  CEO,  SELF, FRED, JILL, Enable, sw[15:0],
     output Y
 );
-
+    
   assign Y = (CEO & ~Sel[0] & ~Sel[1] |
               SELF &  Sel[0] & ~Sel[1] |
               FRED & ~Sel[0] &  Sel[1] |
               JILL &  Sel[0] &  Sel[1] |) & Enable;
 
 endmodule
+
 
