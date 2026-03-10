3c67db5f82737f4bd9fef40fd76daadaf82573aa
collingreen-del <collingreen@u.boisestate.edu>
2026-03-10 17:20:29 -0600

    Refactor top module inputs and remove logic
    Removed previous module inputs and logic, added new inputs for switches and LEDs.

--- top.v
+++ top.v
@@ -1,14 +1,10 @@
 module top(
-    input [1:0] Sel,
-    input  CEO,  SELF, FRED, JILL, Enable, sw[15:0],
+    input sw[15:0], led[15:0]
     output Y
 );
     
-  assign Y = (CEO & ~Sel[0] & ~Sel[1] |
-              SELF &  Sel[0] & ~Sel[1] |
-              FRED & ~Sel[0] &  Sel[1] |
-              JILL &  Sel[0] &  Sel[1] |) & Enable;
 
 endmodule
+
 
 
